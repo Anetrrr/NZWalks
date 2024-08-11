8b64f2e527d7510101255676ac024738820c73bc
Anetor <anetor.ataga@gmail.com>
2024-08-11 02:01:48 +0100

    complete endpoints for Region

--- NZWalks/Controllers/RegionsController.cs
+++ NZWalks/Controllers/RegionsController.cs
@@ -13,7 +13,7 @@ namespace NZWalks.Controllers
     {
         private readonly NZWalksDbContext dbContext;
 
-        public RegionsController(NZWalksDbContext dbContext) 
+        public RegionsController(NZWalksDbContext dbContext)
         {
             this.dbContext = dbContext;
         }
@@ -27,7 +27,7 @@ namespace NZWalks.Controllers
         public IActionResult GetAll()
         {
             //Get Data from Database - Domain Models.
-           var regionsDomain = dbContext.Regions.ToList();
+            var regionsDomain = dbContext.Regions.ToList();
 
             //Map Domain Models to DTOs - we return DTOs back to the client, not the Domain Models
             var regionsDto = new List<RegionDTO>();
@@ -60,9 +60,9 @@ namespace NZWalks.Controllers
 
             var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
 
-            
 
-            if(regionDomain == null)
+
+            if (regionDomain == null)
             {
                 return NotFound();
             }
@@ -80,5 +80,92 @@ namespace NZWalks.Controllers
 
         }
 
+        //Post to Create New region
+        //POST: https://localhost:portnumber/api/regions
+
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+
+            // Map or convert dto to domain model
+
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+
+            };
+
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges();
+
+            //Map domain model back to dto
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id }, regionDto);
+        }
+
+        //Update Region
+        //PUT:https://localhost:portnumber/api/regions/{id}
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomain == null)
+            {
+                return NotFound();
+
+            }
+
+            regionDomain.Name = updateRegionRequestDto.Name;
+            regionDomain.Code = updateRegionRequestDto.Code;
+            regionDomain.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+
+            dbContext.SaveChanges();
+
+            //mapping back to dto
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+
+            };
+
+            return Ok(regionDto);
+
+        }
+
+        //Deleting a region
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomain == null)
+            {
+                return NotFound();
+
+            }
+            dbContext.Regions.Remove(regionDomain);
+            dbContext.SaveChanges();
+
+            return Ok("Successfully deleted");
+        }
     }
+
 }
---
+++ NZWalks/Models/DTO/AddRegionRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NZWalks.Models.DTO
+{
+    public class AddRegionRequestDto
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionImageUrl { get; set; }
+    }
+}
